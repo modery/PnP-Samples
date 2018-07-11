95e34ef28b181e75cb09f27639d61663f29ee7d5
Rene Modery <rene@modery.net>
2018-07-11 17:50:34 +0800

    Update GetPermissionLevels.ps1
    Improved output to CSV

--- Permissions/GetPermissionLevels.ps1
+++ Permissions/GetPermissionLevels.ps1
@@ -5,6 +5,7 @@ $tenantPrefix = "mytenant"
 $cred = Get-Credential
 Connect-PnPOnline -Url "https://$($tenantPrefix)-admin.sharepoint.com" -Credentials $cred
 $sites = Get-PnPTenantSite | where {$_.Url -like "*$($tenantPrefix).sharepoint.com/sites/*" -or $_.Url -like "*$($tenantPrefix).sharepoint.com/teams/*"}
+$allPermissionLevels = @()
 foreach($site in $sites) {
     Connect-PnPOnline -Url $site.Url -Credentials $cred
     $ctx = Get-PnPContext
@@ -12,6 +13,10 @@ foreach($site in $sites) {
     $ctx.Load($roleDefs)
     $ctx.ExecuteQuery()
     foreach($rd in $roleDefs) {
-        Add-Content ".\permissionlevels.csv" "$($site.url), $($rd.Name)"
+        $obj = New-object -typename PSObject ` |
+               Add-Member -memberType NoteProperty -name SiteUrl -value $site.url -passthru | `
+               Add-Member -memberType NoteProperty -name PermissionLevel -value $rd.Name -passthru
+        $allPermissionLevels += $obj
     }
 }
+$allPermissionLevels | Export-Csv ".\permissionlevels.csv" -NoTypeInformation
